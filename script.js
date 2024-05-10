ad6ceaa2cb415d1613f6ffe3bb4b2d001c40a7bc
Mari0mori <ardutah9@gmail.com>
2024-05-10 03:30:48 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+    $('#clickMe').click(function() {
+        $('#contentContainer').toggleClass('darkMode');
+    });
+
+    const storeItems = [
+        {
+            name: 'TV',
+            price: 800.00,
+            inStock: true,
+            details: '4K Ultra HD'
+        },
+        {
+            name: 'Phone',
+            price: 700.00,
+            inStock: false,
+            details: '5G'
+        },
+        {
+            name: 'Game Console',
+            price: 300.00,
+            inStock: true,
+            details: 'The latest and greatest'
+        },
+        {
+            name: 'Laptop',
+            price: 1200.00,
+            inStock: true,
+            details: '16GB RAM 1TB SSD'
+        },
+        {
+            name: 'Smart Watch',
+            price: 200.00,
+            inStock: false,
+            details: 'Counts your steps'
+        },
+        {
+            name: 'Headphones',
+            price: 100.00,
+            inStock: true,
+            details: 'Clearest music to be heard'
+        },
+        {
+            name: 'Keyboard',
+            price: 100.00,
+            inStock: true,
+            details: 'Types for you'
+        },
+        {
+            name: 'HDMI Cord',
+            price: 100.00,
+            inStock: true,
+            details: 'HDMI to USB type C'
+        },
+        {
+            name: 'Monitor',
+            price: 300.00,
+            inStock: true,
+            details: '4K Ultra HD'
+        },
+        {
+            name: 'Speaker',
+            price: 200.00,
+            inStock: true,
+            details: 'Clearest music to be heard'
+        },
+        {
+            name: 'Video Game',
+            price: 60.00,
+            inStock: true,
+            details: 'Enjoy for hours'
+        },
+    ];
+    const productList = $('#productList');
+    storeItems.forEach(function(item) {
+        if (item.inStock) {
+            productList.append(
+                `<div class="product">
+                    <p>Name: ${item.name}</p>
+                    <p>Price: $${item.price}</p>
+                    <p>Details: ${item.details}</p>
+                </div>`
+            );
+        }
+    });
+});
